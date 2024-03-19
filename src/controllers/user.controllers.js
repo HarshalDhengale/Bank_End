ba9497e006eda66bb95363052ddf329fb467cf2c
harshaldhengale <dhengaleharshal@gmial.com>
2024-03-19 23:14:52 +0530

    Add subscription Model

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,8 +3,7 @@ import { ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import{ uploadOnCloudinary } from "../utils/cloudinary.js" 
 import { ApiResponse } from "../utils/ApiResponse.js"
-import { Jwt } from "jsonwebtoken"
-import { app } from "../app.js"
+
 
 const generateAccessAndRefreshTokens = async(userId) =>{
     try {
